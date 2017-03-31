e54e51ca96b3b3c49d7ad461e230031670e64b84
eliancruz29 <eliancruz29@gmail.com>
2017-03-31 16:43:24 -0400

    finnish the two first courses

---
+++ Mobile Certification/Introduction to Xamarin.Android [AND101]/TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            calculateButton.Click += OnCalculateClick;
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+        }
+
+        void OnCalculateClick(object sender, EventArgs e)
+        {
+            string text = inputBill.Text;
+            var bill = double.Parse(text);
+
+            var tip = bill * 0.15;
+            var total = bill + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
+        }
+    }
+}
+
