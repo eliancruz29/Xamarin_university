8f1fa63592c30b14a1e2bf12f810b3eda9cc29ca
eliancruz29 <eliancruz29@gmail.com>
2017-04-24 19:19:21 -0400

    finishing the [XAM130] course

---
+++ MobileCertification/[XAM130]/Calculator/Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            OnClear(this, null);
+        }
+
+        void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if (resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            resultText.Text += pressed;
+
+            double number;
+            if (double.TryParse(resultText.Text, out number))
+            {
+                resultText.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+        void OnSelectOperator(object sender, EventArgs e)
+        {
+            currentState = -2;
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
+        void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            resultText.Text = "0";
+        }
+
+        void OnCalculate(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {
+                double result = 0;
+                switch (mathOperator)
+                {
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        break;
+                    case "X":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    default:
+                        break;
+                }
+
+                firstNumber = result;
+                resultText.Text = result.ToString("N0");
+                currentState = -1;
+            }
+        }
+    }
+}
