ab53fef7aa3e4d8ce818c1e9085272f94e9d5119
eliancruz29 <eliancruz29@gmail.com>
2017-05-02 16:03:56 -0400

    change to finish the exercise #3 [XAM160]

---
+++ MobileCerti/[XAM160]/XAM160-ex3-start/People/People/App.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace People
+{
+	public class App : Application
+	{
+        public static PersonRepository PersonRepo { get; private set; }
+
+        public App(string dbPath)
+        {
+            PersonRepo = new PersonRepository(dbPath);
+
+            this.MainPage = new MainPage();
+
+            // The root page of your application
+   //         MainPage = new ContentPage
+			//{
+			//	Content = new StackLayout
+			//	{
+			//		VerticalOptions = LayoutOptions.Center,
+			//		Children = {
+			//			new Label {
+			//				HorizontalTextAlignment = TextAlignment.Center,
+			//				Text = dbPath
+   //                     }
+			//		}
+			//	}
+			//};
+		}
+		
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
