a13e776e515a0316b4e4286f5b8831d2ba7f247d
eliancruz29 <eliancruz29@gmail.com>
2017-05-02 13:10:14 -0400

    change to finish the exercise #1 [XAM160]

---
+++ MobileCerti/[XAM160]/xam160-ex1-start/People/People/People/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace People
+{
+    public partial class App : Application
+    {
+        //public App()
+        //{
+        //    InitializeComponent();
+
+        //    MainPage = new People.MainPage();
+        //}
+
+        public App(string displayText)
+        {
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = displayText
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
