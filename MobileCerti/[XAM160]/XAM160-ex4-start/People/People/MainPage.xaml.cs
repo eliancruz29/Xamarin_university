fe2dca2ff801932fc59b6e200c93774e2f9b6744
eliancruz29 <eliancruz29@gmail.com>
2017-05-02 19:50:05 -0400

    change to finish the exercise #4 in the entire course [XAM160]

---
+++ MobileCerti/[XAM160]/XAM160-ex4-start/People/People/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using People.Models;
+using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace People
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async Task OnNewButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            await App.PersonRepo.AddNewPersonAsync(newPerson.Text);
+            statusMessage.Text = App.PersonRepo.StatusMessage;
+        }
+
+        public async Task OnGetButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            ObservableCollection<Person> people =
+                new ObservableCollection<Person>(
+                    await App.PersonRepo.GetAllPeopleAsync());
+            peopleList.ItemsSource = people;
+        }
+    }
+}
